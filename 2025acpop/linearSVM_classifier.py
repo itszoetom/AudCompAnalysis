93d7d85018edb8aeb909554843c6053cf508596d
zoebtomlinson1@gmail.com <itszoetom>
2025-11-03 10:53:53 -0800

    changed c values from all positive logspace to negative logspace

--- 2025acpop/linearSVM_classifier.py
+++ 2025acpop/linearSVM_classifier.py
@@ -20,7 +20,7 @@ colors = {
     'Primary auditory area': '#2ca02c',
     'Ventral auditory area': '#d62728'}
 
-hyperparameters = np.logspace(-3, 2, 30)
+hyperparameters = np.logspace(-4, 1, 20)
 boxplot_data = {}
 all_results = []
 
