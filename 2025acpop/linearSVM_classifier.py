62a0ed83692ffe34f396e2817268d77c774a035c
zoebtomlinson1@gmail.com <itszoetom>
2025-11-03 13:57:07 -0800

    larger c logspace

--- 2025acpop/linearSVM_classifier.py
+++ 2025acpop/linearSVM_classifier.py
@@ -20,7 +20,7 @@ colors = {
     'Primary auditory area': '#2ca02c',
     'Ventral auditory area': '#d62728'}
 
-hyperparameters = np.logspace(-8, 5, 20)
+hyperparameters = np.logspace(-8, 8, 20)
 boxplot_data = {}
 all_results = []
 
